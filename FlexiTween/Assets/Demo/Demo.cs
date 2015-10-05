53cc13b061ae7a919298c326c61a562ee73454bf
Yuriy Gromchenko <gromchen@outlook.com>
2015-10-05 13:57:58 +0300

    Added demo scene

---
+++ FlexiTween/Assets/Demo/Demo.cs
@@ -0,0 +1,40 @@
+﻿using FlexiTween;
+using FlexiTween.Extensions;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    [SerializeField] private Vector3 targetPosition = new Vector3(0, 3, 0);
+    [SerializeField] private float duration = 1;
+    [SerializeField] private AnimationCurve curve = AnimationCurveHelper.GetLinearCurve();
+
+    private ITween _tween;
+    private bool _atTarget;
+    private Vector3 _originalPosition;
+
+    private void Start()
+    {
+        _originalPosition = transform.position;
+
+        TweenPosition();
+    }
+
+    private void TweenPosition()
+    {
+        var position = _atTarget
+            ? _originalPosition
+            : targetPosition;
+
+        _atTarget = !_atTarget;
+
+        _tween.SafelyAbort();
+        _tween = Tween
+            .From(transform.position)
+            .To(position, duration)
+            .OnUpdate(p => transform.position = p)
+            .Easing(curve)
+            .Start();
+
+        _tween.OnComplete += TweenPosition;
+    }
+}
